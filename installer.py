35a52cd4accbcbea3739dd00c3ecd77b07fd7f03
SkyKrye <55189575+SkyKrye@users.noreply.github.com>
2023-04-19 12:41:56 +0500

    installer for modules
    I made a seperate installer for the modules required to run the launcher

---
+++ installer.py
@@ -0,0 +1,21 @@
+#installer
+
+import subprocess
+import time
+
+#subprocess.run(["pip", "install", "minecraft-launcher-lib"])
+#subprocess.run(["pip", "install", "argparse"])
+#subprocess.run(["pip", "install", "tkinter"])
+#subprocess.run(["pip", "list"])
+#time.sleep(10)
+
+print('Installation done!')
+print("A messagebox should show up for you on screen, if it doesn't, then that means that tkinter was configured incorrectly")
+
+from tkinter import *
+from tkinter import messagebox
+
+messagebox.showinfo("skypy-launcher", """The following modules have been installed successfully:
+> minecraft-launcher-lib
+> argparse
+> tkinter""")
